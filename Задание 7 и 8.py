6ab1f7a0625be110dd35641952f63ddcc0144af5
woodiergore <nikita.schaimordanov@gmail.com>
2025-10-30 00:29:16 +0300

    задания готовы

--- Задание 7 и 8.py
+++ Задание 7 и 8.py
@@ -1,3 +1,22 @@
-for i in range(26):
-    for j in range(26):
-        print(f"{chr(ord('a') + i)}{chr(ord('a') + j)}")
+print("6 ноября 2025")
+print("ЗАДАНИЕ 1")
+a = int(input("Введите длину прямоугольника: "))
+b = int(input("Введите ширину прямоугольника: "))
+
+for i in range(b):
+    for j in range(a):
+        print("*", end='')
+    print()
+
+print()
+print("ЗАДАНИЕ 2")
+for i in range(2, 10):
+    for j in range(2, 10):
+        if i%2==0 or j%2==0:
+            continue
+        elif i*j >= 50:
+            break
+        elif i*j == 25:
+            pass
+        else:
+            print(f'{i} * {j} = {i*j}')
